92a36966121ee9d67f838775358477178522e25d
alamgirweb11 <alamgirweb11@gmail.com>
2022-06-06 23:37:53 +0600

    user login method added

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     private $successCode = 200;
+    // register user
     public function register(Request $request){
             // validate data
             $this->validate($request,[
@@ -29,10 +30,28 @@ class UserController extends Controller
         try{
              User::create($user);
              $message['message'] = 'Registration successful';
-             return response()->json(['success' => $message], $this->successCode);
+             return response()->json(['success' => $message], 201);
         }catch(Exception $e){
               $message['message'] = $e->getMessage();
               return response()->json(['success' => $message], $this->getCode());
         }
+    }
+
+    // login user
+    public function login(Request $request){
+         $this->validate($request, [
+              'email' => 'required|email',
+              'password' => 'required'
+         ]);
+
+         if(!$token = auth()->attempt(['email' => $request->email, 'password' => $request->password])){
+                $message['message'] = 'The given credentials is incorrect!';
+                return response()->json(['error' => $message], 422);
+         }
+         $message['message'] = 'Successfully Login';
+         return response()->json([
+              'message' => $message,
+              'token' => $token,
+         ], $this->successCode);
     }
 }

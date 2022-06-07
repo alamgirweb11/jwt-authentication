b996821da64fd9efffd4df0ba318bc0fdcda67ba
alamgirweb11 <alamgirweb11@gmail.com>
2022-06-07 22:33:01 +0600

    make post CRUD for test authentication

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,82 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
+
+    // post list
+    public function index(){
+             $user_id = Auth::id();
+             $is_exists = Post::where('user_id', $user_id)->exists();
+             if(!$is_exists){
+                  $message['message'] = 'Data not found!';
+                  return response()->json([
+                        'message' => $message,
+                  ], 404);
+             }
+
+            $posts = Post::whereIn('user_id', [$user_id])->get();
+
+            $message['message'] = 'Great! Data found.';
+          return response()->json([
+                'message' =>  $message,
+                'posts' => $posts
+          ], 200);
+    }
+
+    // store post
+    public function store(Request $request){
+               $this->validate($request, [
+                     'title' => 'required|string',
+                     'description' => 'required',
+               ]);
+
+             $input = $request->all();
+             $input['user_id'] = Auth::id();
+         try{
+              Post::create($input);
+              $message['message'] = 'Post created successfully';
+              return response()->json(['message' => $message], 200);
+         }catch(Exception $e){
+            $message['message'] = $e->getMessage();
+            return response()->json(['message' => $message], $e->getCode());
+         }
+    }
+
+    // update post
+    public function update(Request $request, $id){
+               $this->validate($request, [
+                     'title' => 'required|string',
+                     'description' => 'required',
+               ]);
+             
+               $post = Post::findOrFail($id);
+
+         try{
+             $post->update($request->all());
+              $message['message'] = 'Post updated successfully';
+              return response()->json(['message' => $message], 200);
+         }catch(Exception $e){
+            $message['message'] = $e->getMessage();
+            return response()->json(['message' => $message], $e->getCode());
+         }
+    }
+
+    // delete post
+    public function destroy($id){
+        $post = Post::findOrFail($id);
+        try{
+            $post->delete();
+             $message['message'] = 'Post deleted successfully';
+             return response()->json(['message' => $message], 200);
+        }catch(Exception $e){
+           $message['message'] = $e->getMessage();
+           return response()->json(['message' => $message], $e->getCode());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth:api']], function(){
        Route::get('profile', [UserController::class, 'profile']);
        Route::get('logout', [UserController::class, 'logout']);
+       // posts route
+       Route::get('post-list', [PostController::class, 'index']);
+       Route::post('post-store', [PostController::class, 'store']);
+       Route::put('post-update/{id}', [PostController::class, 'update']);
+       Route::delete('post-delete/{id}', [PostController::class, 'destroy']);
 });
 
 Route::post('register', [UserController::class, 'register']);
